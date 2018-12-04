6887097af17046598a6ff157a48163ebbe10b958
Andrew Kane <andrew@chartkick.com>
2018-12-04 03:27:18 -0800

    Cache version

--- test/ci/install_libsodium.sh
+++ test/ci/install_libsodium.sh
@@ -4,13 +4,13 @@ set -e
 
 LIBSODIUM_VERSION=1.0.16
 
-if [ ! -d "$HOME/libsodium/lib" ]; then
+if [ ! -d "$HOME/libsodium/$LIBSODIUM_VERSION" ]; then
   wget "https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VERSION.tar.gz"
   tar xvfz "libsodium-$LIBSODIUM_VERSION.tar.gz"
   cd "libsodium-$LIBSODIUM_VERSION"
-  ./configure --prefix=$HOME/libsodium
+  ./configure --prefix=$HOME/libsodium/$LIBSODIUM_VERSION
   make && make check
   make install
 else
-  echo "Libsodium $LIBSODIUM_VERSION cached"
+  echo "Libsodium cached"
 fi
