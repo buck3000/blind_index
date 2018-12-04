5f6076e257ee114f47155d14d0504ce2de7afd6c
Andrew Kane <andrew@chartkick.com>
2018-12-04 03:28:41 -0800

    Cache version

--- test/ci/install_libsodium.sh
+++ test/ci/install_libsodium.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-LIBSODIUM_VERSION=1.0.16
-
 if [ ! -d "$HOME/libsodium/$LIBSODIUM_VERSION" ]; then
   wget "https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VERSION.tar.gz"
   tar xvfz "libsodium-$LIBSODIUM_VERSION.tar.gz"
