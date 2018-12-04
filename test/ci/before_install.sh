9bec7198437487c6dd78fe50e09bdc7f8903a7c5
Andrew Kane <andrew@chartkick.com>
2018-12-04 03:05:58 -0800

    Cache libsodium

--- test/ci/before_install.sh
+++ test/ci/before_install.sh
@@ -1,7 +1,16 @@
-# install latest libsodium
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.16.tar.gz
-tar xvfz libsodium-1.0.16.tar.gz
-cd libsodium-1.0.16
-./configure
-make && make check
-sudo make install
+#!/bin/sh
+
+set -e
+
+LIBSODIUM_VERSION="1.0.16"
+CACHE_DIR="$HOME/libsodium/$LIBSODIUM_VERSION"
+
+if [ ! -d $CACHE_DIR ]; then
+  mkdir -p $CACHE_DIR
+  wget "https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VERSION.tar.gz"
+  tar xvfz "libsodium-$LIBSODIUM_VERSION.tar.gz"
+  cd "libsodium-$LIBSODIUM_VERSION"
+  ./configure --prefix=$CACHE_DIR
+  make && make check
+  make install
+fi
